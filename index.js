3fe7a0109b6c7557a9108b5d260b0aa3ff712d07
GoUnstoppable® <gounstoppableyt@gmail.com>
2021-05-22 14:31:44 +0530

    Leap year Checker Tool in JS

---
+++ index.js
@@ -0,0 +1,10 @@
+function check() {
+  var year = document.getElementById("year").value;
+  // var btn = document.getElementById("check");
+  var yearName = document.getElementById("year").value;
+  if (year % 4 == 0) {
+    document.getElementById("result").innerHTML = yearName + " is a Leap Year.";
+  } else {
+    document.getElementById("result").innerHTML = yearName + " is not a Leap Year.";
+  }
+}

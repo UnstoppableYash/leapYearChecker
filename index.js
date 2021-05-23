b0519107ddc5ac727c64fa467dca929a9e6518f8
GoUnstoppable® <gounstoppableyt@gmail.com>
2021-05-23 15:07:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ function check() {
   var year = document.getElementById("year").value;
   // var btn = document.getElementById("check");
   var yearName = document.getElementById("year").value;
-  if (year % 4 == 0) {
+  if (year % 100 == 0) {
+    document.getElementById("result").innerHTML = yearName + " is not a Leap Year.";
+  } else if (year % 4 == 0) {
     document.getElementById("result").innerHTML = yearName + " is a Leap Year.";
   } else {
-    document.getElementById("result").innerHTML = yearName + " is not a Leap Year.";
+    document.getElementById("result").innerHTML =
+      yearName + " is not a Leap Year.";
   }
 }
+
